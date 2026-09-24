03a7c74ba364e3ef28821aeacaee5af55607909f
davidoberst <juandavidbustosperez82@gmail.com>
2026-09-24 00:38:35 -0500

    flujo completo hasta adjuntar imagen, problema de captcha

--- google_images_scrapper.py
+++ google_images_scrapper.py
@@ -1,9 +1,8 @@
-
 import asyncio
 from playwright.async_api import async_playwright
 
-
 website = "https://images.google.com/"
+img = input('img : ')
 
 async def main():
     async with async_playwright() as p:
@@ -12,15 +11,20 @@ async def main():
         page = await context.new_page()
         
         print("[+] Loading Google Images...")
-        await page.goto(website, wait_until="domcontentloaded", timeout=60000) #evitar bloqueos 
+        await page.goto(website, wait_until="domcontentloaded", timeout=60000)
         
-        await page.locator(".hWdRGb").click()
+        await page.locator(".hWdRGb").click() # localizador de icono de camara
         await page.wait_for_timeout(2000)
-        await page.locator(".DV7the").click()
+       
         print('[+] Uploading image...')
+        
+        # Corregida la indentación (debe estar dentro de main y del contexto del navegador)
+        async with page.expect_file_chooser() as fc_info:
+            await page.locator(".DV7the").click()
+            
+        file_chooser = await fc_info.value
+        await file_chooser.set_files(img)
+        
         await page.wait_for_timeout(10000)
 
-        await browser.close()
-
 asyncio.run(main())
-        
